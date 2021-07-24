940f4df0ad8f7a39937d75ba330eec34d442eb27
GladeCant <79660487+GladeCant@users.noreply.github.com>
2021-07-24 18:46:32 +0200

    [Maj n°1] Add join & leave defaultSettings

--- util/config.js
+++ util/config.js
@@ -2,6 +2,14 @@ module.exports = {
   token: 'azTNoNcEcInEsTpAsUnToKeN.YLPr1A.Jip8GRKF8md4lol4tzSAkR98_SA',
   dbconnection: 'mongodb+srv://Tsoin_tsoin:tralala@discordbot.hfgmg.mongodb.net/DiscordBot?retryWrites=true&w=majority',
   defaultSettings: {
-    prefix: "k!"
-  },
+    prefix: "k!",
+    join: {
+      channel: null,
+      message: null
+    },
+    leave: {
+      channel: null,
+      message: null
+    }
+  }
 }
