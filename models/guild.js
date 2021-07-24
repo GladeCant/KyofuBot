827199604acdea3df1339edc72c8da0ed6a36514
GladeCant <79660487+GladeCant@users.noreply.github.com>
2021-07-24 18:45:21 +0200

    [Maj n°1] Add join & leave options

--- models/guild.js
+++ models/guild.js
@@ -1,10 +1,19 @@
+const { MessageEmbed } = require('discord.js');
 const { model, Schema } = require('mongoose');
 
 const guildSchema = Schema({
   _id: Schema.Types.ObjectId,
   guildID: String,
   guildName: String,
-  prefix: String
+  prefix: String,
+  join: {
+    channel: String,
+    message: Schema.Types.Mixed
+  },
+  leave: {
+    channel: String,
+    message: Schema.Types.Mixed
+  }
 });
 
 module.exports = model('Guild', guildSchema);
